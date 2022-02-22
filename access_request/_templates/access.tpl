5fd36b73c7836a712b41d0948d9113aa7f7c8446
rajeshwer rao <rajeshwerrao@rajeshwers-MacBook-Pro.local>
2022-02-22 15:46:37 -0800

    request access & revoke access functions

--- access_request/_templates/access.tpl
+++ access_request/_templates/access.tpl
@@ -1,5 +1,6 @@
 {
 	"aws_username": "${USERNAME}",
-	"timeDuration_in_seconds": "${DURATION}"
+	"timeDuration_in_seconds": "${DURATION}",
+	"Reason for Prod access": "${REASON}"
 
 }
--- access_request/access.sh
+++ access_request/access.sh
@@ -4,46 +4,70 @@ set -euo pipefail
 
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-while [[ $# -gt 0 ]]
-do
+while [[ $# -gt 0 ]]; do
     key="$1"
     case "$key" in
-        -t|--template)
-            tpl="$2"
-            shift
-            shift
-            ;;
-        -u|--username)
-            username="${2}"
-            shift
-            shift
-            ;;
-        -d|--duration)
-            duration="${2}"
-            shift
-            shift
-            ;;      
-        *)
-            echo "unknown option: ${key}"
-            exit 1
-            ;;
+    -t | --template)
+        tpl="$2"
+        shift
+        shift
+        ;;
+    -u | --username)
+        username="${2}"
+        shift
+        shift
+        ;;
+    -d | --duration)
+        duration="${2}"
+        shift
+        shift
+        ;;
+    *)
+        echo "unknown option: ${key}"
+        exit 1
+        ;;
     esac
 done
 
 tpl=${tpl:-"access"}
 if [[ -z ${username+guard} ]]; then
-  echo "aws account username must be specified through -u|--username"
-  exit 1
+    echo "aws account username must be specified through -u|--username"
+    exit 1
 fi
 if [[ -z ${duration+guard} ]]; then
-  echo "expected duration for access in seconds must be  specified through -d|--duration"
-  exit 1
+    echo "expected duration for access in seconds must be  specified through -d|--duration"
+    exit 1
 fi
+
+echo "Please select reason for access from the following"
+options=("Pager" "jira_ticket" "bugfix" "Restart_service/Pod" "quit")
+select opt in "${options[@]}"; do
+    case $opt in
+    "Pager")
+        break
+        ;;
+    "jira_ticket")
+        break
+        ;;
+    "bugfix")
+        break
+        ;;
+    "Restart_service/Pod")
+        break
+        ;;
+    "quit")
+        break
+        ;;
+    *) echo "invalid option $REPLY" ;;
+    esac
+done
 
 export ACCESS_DATE=$(date '+%Y-%m-%d-%H:%M:%S%z')
 export USERNAME=${username}
 export DURATION=${duration}
+export REASON=$opt
 echo $USERNAME
 echo $DURATION
+echo $REASON
 
-cat $ROOT/_templates/${tpl}.tpl | envsubst > $ROOT/accessRequest/${ACCESS_DATE}-access.json
+cat $ROOT/_templates/${tpl}.tpl | envsubst >$ROOT/accessRequest/${ACCESS_DATE}-access.json
